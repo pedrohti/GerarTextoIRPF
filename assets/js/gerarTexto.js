ed1594caaf0c055a8660510258c0c7663ae08aa4
PedroHMoraes <pedroh.ti@hotmail.com>
2022-03-06 15:14:42 -0300

    finish

--- assets/js/gerarTexto.js
+++ assets/js/gerarTexto.js
@@ -1,18 +1,11 @@
-$(document).ready(function () {
-	$.getJSON("/assets/plataformas.json", function (data) {
-		data.forEach((e, i) => {
-			let checked = i == 0 ? "checked='checked'" : "";
+var plataformas = ["Clear", "Mercado Bitcoin", "Binance", "Rico"];
 
-			$("#bttRadios").append(
-				`<div class="form-check">
-				<input class="form-check-input" type="radio" name="plataforma" value=${i} ${checked}>${e.plataforma}</input>
-			</div>`
-			);
-		});
-	}).fail(function () {
-		alert("Não foi possivel ler o JSON.");
-	});
-});
+var cnpjs = {
+	0: "Na Clear Corretora (02.332.886/0011-78)",
+	1: "No Mercado Bitcoin (18.213.434/0001-35)",
+	2: "Na Binance (37.512.394/0001-77)",
+	3: "Na Rico Corretora (02.332.886/0016-82)",
+};
 
 var empresa = $("#txtEmpresa"),
 	codigo = $("#txtCodigo"),
@@ -21,46 +14,48 @@ var empresa = $("#txtEmpresa"),
 	gerar = $("#bttGerar"),
 	resultado = $("#lblResultado");
 
-const getPlataform = (opt) => {
-	var plataforma;
-	// $.getJSON("/assets/plataformas.json", function (data) {
-	// 	data.forEach((e, i) => {
-	// 		// if (i == opt) console.log(e.texto);
-	// 		plataforma = i == opt ? e.texto : "";
-	// 	});
-	// }).fail(function () {
-	// 	alert("Não foi possivel ler o JSON.");
-	// });
+$(document).ready(function () {
+	plataformas.forEach((e, i) => {
+		let checked = i == 0 ? "checked='checked'" : "";
 
-	// console.log(opt);
-	let platformas = {
-		0: "Na Clear Corretora (02.332.886/0011-78)",
-		1: "No Mercado Bitcoin (18.213.434/0001-35)",
-		2: "Na Binance (37.512.394/0001-77)",
-		3: "Na Rico Corretora (02.332.886/0016-82)",
-	};
+		$("#bttRadios").append(
+			`<div class="form-check">
+				<input class="form-check-input" type="radio" name="plataforma" value=${i} ${checked}>${e}</input>
+			</div>`
+		);
+	});
+});
 
-	return platformas[opt];
+const getPlataforma = (opt) => {
+	return cnpjs[opt];
 };
 
-function copyToClipboard() {
-	var copyText = document.getElementById("lblResultado").textContent;
-	navigator.clipboard.writeText(copyText);
+function copyToClipboard(texto) {
+	navigator.clipboard.writeText(texto);
 }
+
+var formatter = new Intl.NumberFormat("pt-BR", {
+	style: "currency",
+	currency: "BRL",
+});
 
 gerar.click((e) => {
 	e.preventDefault();
+
 	let radio = $(".form-check-input:checked")
 		.map(function () {
 			return $(this).val();
 		})
 		.get();
 
-	let plataforma = getPlataform(radio[0]);
+	let plataforma = getPlataforma(radio[0]);
 
-	let totalPago = Math.round(qtd.val() * precoMedio.val().replace(",", "."));
-	let texto = `${qtd.val()} ações de ${empresa.val()} (${codigo.val()}). A um preço médio de R$ ${precoMedio.val()} e custo total de R$ ${totalPago}. ${plataforma}`;
+	let totalPago = qtd.val() * precoMedio.val().replace(",", ".");
+	let texto =
+		`${qtd.val()} ações de ${empresa.val()} (${codigo.val()}). A um custo total de ${formatter.format(
+			totalPago
+		)}. ${plataforma}`.toUpperCase();
 
 	resultado.text(texto);
-	copyToClipboard();
+	copyToClipboard(texto);
 });
